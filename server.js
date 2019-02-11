efc1333d5ed45a99fdf11d67b2dd7ef4d0df0137
Marc Flerackers <mflerackers@gmail.com>
2019-02-11 20:17:23 +0900

    New query builder, new/edit form

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser= require('body-parser')
 const mongodb = require('mongodb');
 const app = express();
 const stats = require("./stats")
+const fieldNames = require("./field_names")
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -22,6 +23,17 @@ mongodb.connect('mongodb://localhost:27017', {
   })
 })
 
+const queryNames = [
+    "article",
+    "list",
+    "count",
+    "group",
+    "map",
+    "search",
+    "new",
+    "edit"
+];
+
 app.get('/', function(req, res) {
     db.collection('thaidb').find().toArray((err, result) => {
         if (err) return console.log(err);
@@ -36,33 +48,24 @@ app.get('/article/:id', function(req, res) {
     });
 })
 
-const map = {
-    newspaper: "$bibliography.newspaper",
-    geo: "$categories.place.geo",
-    "external-factor": "$categories.happening.external-factor",
-    density: "$categories.place.density",
-    topic: "$categories.topics",
-    keyword: "$article.keywords",
-    product: "$categories.product.kind",
-    province: "$categories.place.province",
-    gender: "$categories.people.gender",
-    education: "$categories.people.education-level",
-    age: "$categories.people.age",
-    work: "$categories.people.work-specific",
-};
-
 app.get('/list/:name', function(req, res) {
-    let name = (map[req.params.name] || req.params.name).slice(1);
+    let name = req.params.name;
     db.collection('thaidb').distinct(name, (err, result) => {
         if (err) return console.log(err);
         result = result.filter(article => article && article._id != "");
         result.sort();
-        res.render('list.ejs', {articles:result, attribute:req.params.name, title:`Distinct ${req.params.name}`});
+        res.render('list.ejs', {
+            articles:result, 
+            attribute:req.params.name, 
+            title:`Distinct ${req.params.name}`,
+            queryNames:queryNames,
+            fieldNames:fieldNames
+        });
     });
 })
 
 app.get('/list/:name/:value', function(req, res) {
-    let name = (map[req.params.name] || req.params.name).slice(1);
+    let name = req.params.name;
     let aggregate = [];
     if (name.startsWith("categories.happening")) {
         aggregate.push({$unwind:"$categories.happening"});
@@ -79,7 +82,12 @@ app.get('/list/:name/:value', function(req, res) {
     //db.collection('thaidb').find({[name]: req.params.value}).toArray((err, result) => {
         if (err) return console.log(err);
         result = result.filter(article => article && article._id != "");
-        res.render('index.ejs', {articles:result, title:`${req.params.name} - ${req.params.value}`});
+        res.render('index.ejs', {
+            articles:result, 
+            title:`${req.params.name} - ${req.params.value}`,
+            queryNames:queryNames,
+            fieldNames:fieldNames
+        });
     });
 })
 
@@ -87,25 +95,32 @@ app.get('/search', function(req, res) {
     db.collection('thaidb').find({ $text: { $search: req.query.query } }, {"article.abstract":1, _id:1}).toArray((err, result) => {
         if (err) return console.log(err);
         result.sort();
-        res.render('index.ejs', {articles:result, attribute:req.params.name, title:req.params.name});
+        res.render('index.ejs', {
+            articles:result, 
+            attribute:req.params.name, 
+            title:req.params.name,
+            queryNames:queryNames,
+            fieldNames:fieldNames
+        });
     });
 })
 
 app.get('/count/:name', function(req, res) {
-    let group = (map[req.params.name] || req.params.name);
+    let group = req.params.name;
     let aggregate = [];
-    if (group.startsWith("$categories.happening")) {
+    if (group.startsWith("categories.happening")) {
         aggregate.push({$unwind:"$categories.happening"});
     }
-    if (group == "$categories.topics") {
+    if (group == "categories.topics") {
         aggregate.push({$unwind:"$categories.topics"});
     }
-    if (group == "$article.keywords") {
+    if (group == "article.keywords") {
         aggregate.push({$unwind:"$article.keywords"});
     }
-    if (group.startsWith("$categories.people")) {
+    if (group.startsWith("categories.people")) {
         aggregate.push({$unwind:"$categories.people"});
     }
+    group = "$" + group;
     aggregate.push({$sortByCount:group});
     console.log(group, aggregate);
     db.collection('thaidb').aggregate(aggregate).toArray((err, result) => {
@@ -115,26 +130,36 @@ app.get('/count/:name', function(req, res) {
         let count = result.map(article => article.count);
         let statistics = { stdev: stats.stdevp(count), mean: stats.meanp(count), confidence:stats.confidence(0.05, stats.stdevp(count), count.length) };
         console.log(statistics);
-        res.render('count.ejs', {articles:articles, count:count, attribute:req.params.name, title:`Count by ${req.params.name}`, statistics:statistics});
+        res.render('count.ejs', {
+            articles:articles, 
+            count:count, 
+            attribute:req.params.name, 
+            title:`Count by ${req.params.name}`, 
+            statistics:statistics,
+            queryNames:queryNames,
+            fieldNames:fieldNames
+        });
     });
 })
 
 app.get('/group/:first/:second', function(req, res) {
-    let firstGroup = (map[req.params.first] || req.params.first);
-    let secondGroup = (map[req.params.second] || req.params.second);
+    let firstGroup = req.params.first;
+    let secondGroup = req.params.second;
     let aggregate = [];
-    if ([firstGroup, secondGroup].some(name => name.startsWith("$categories.happening"))) {
+    if ([firstGroup, secondGroup].some(name => name.startsWith("categories.happening"))) {
         aggregate.push({$unwind:"$categories.happening"});
     }
-    if ([firstGroup, secondGroup].some(name => name.startsWith("$categories.topics"))) {
+    if ([firstGroup, secondGroup].some(name => name.startsWith("categories.topics"))) {
         aggregate.push({$unwind:"$categories.topics"});
     }
-    if ([firstGroup, secondGroup].some(name => name.startsWith("$article.keywords"))) {
+    if ([firstGroup, secondGroup].some(name => name.startsWith("article.keywords"))) {
         aggregate.push({$unwind:"$article.keywords"});
     }
-    if ([firstGroup, secondGroup].some(name => name.startsWith("$categories.people"))) {
+    if ([firstGroup, secondGroup].some(name => name.startsWith("categories.people"))) {
         aggregate.push({$unwind:"$categories.people"});
     }
+    firstGroup = "$" + firstGroup;
+    secondGroup = "$" + secondGroup;
     aggregate.push({$sortByCount:{$mergeObjects:{first:firstGroup,second:secondGroup}}});
     console.log(firstGroup, secondGroup, aggregate);
     db.collection('thaidb').aggregate(aggregate).toArray((err, result) => {
@@ -183,18 +208,32 @@ app.get('/group/:first/:second', function(req, res) {
             }
             console.log(articles, labels, categories, datasets);
 
-            res.render('group.ejs', {articles:articles, count:count, first:req.params.first, second:req.params.second, 
-                labels:labels, categories:categories, datasets:datasets});
-            }
+            res.render('group.ejs', {
+                articles:articles, 
+                count:count, 
+                first:req.params.first, 
+                second:req.params.second, 
+                labels:labels, 
+                categories:categories, 
+                datasets:datasets,
+                queryNames:queryNames,
+                fieldNames:fieldNames
+            });
+        }
     });
 })
 
 app.get('/group/:first/:second/:valueFirst/:valueSecond', function(req, res) {
-    let nameFirst = map[req.params.first].slice(1);
-    let nameSecond = map[req.params.second].slice(1);
+    let nameFirst =req.params.first;
+    let nameSecond = req.params.second;
     db.collection('thaidb').find({[nameFirst]: req.params.valueFirst, [nameSecond]: req.params.valueSecond}).toArray((err, result) => {
         if (err) return console.log(err);
-        res.render('index.ejs', {articles:result, title:`${req.params.first}:${req.params.valueFirst} - ${req.params.second}:${req.params.valueSecond}`});
+        res.render('index.ejs', {
+            articles:result, 
+            title:`${req.params.first}:${req.params.valueFirst} - ${req.params.second}:${req.params.valueSecond}`,
+            queryNames:queryNames,
+            fieldNames:fieldNames
+        });
     });
 });
 
@@ -268,19 +307,19 @@ app.get('/correlation/:group/:first/:second', function(req, res) {
 });
 
 app.get('/map/:param/:value', function(req, res) {
-    let group = (map[req.params.param] || req.params.param);
+    let group = req.params.param;
     let aggregate = [];
-    if ([group].some(name => name.startsWith("$categories.happening"))) {
+    if ([group].some(name => name.startsWith("categories.happening"))) {
         aggregate.push({$unwind:"$categories.happening"});
     }
-    if ([group].some(name => name.startsWith("$categories.topics"))) {
+    if ([group].some(name => name.startsWith("categories.topics"))) {
         aggregate.push({$unwind:"$categories.topics"});
     }
-    if ([group].some(name => name.startsWith("$article.keywords"))) {
+    if ([group].some(name => name.startsWith("article.keywords"))) {
         aggregate.push({$unwind:"$article.keywords"});
     }
-    aggregate.push({$match:{[group.slice(1)]:req.params.value}});
-    aggregate.push({$sortByCount:map["geo"]});
+    aggregate.push({$match:{[group]:req.params.value}});
+    aggregate.push({$sortByCount:"$categories.place.geo"});
     console.log(group, aggregate);
     db.collection('thaidb').aggregate(aggregate).toArray((err, result) => {
         if (err) return console.log(err)
@@ -291,7 +330,32 @@ app.get('/map/:param/:value', function(req, res) {
             data[province] = (data[province] || 0) + record.count;
         });
         data = Object.entries(data).map(([province, count]) => ({name:province, count:count}));
-        res.render('map.ejs', {articles:data, attribute:req.params.param, value:req.params.value , title:`Map where ${req.params.param} is ${req.params.value}`});
+        res.render('map.ejs', {
+            articles:data, 
+            attribute:req.params.param, 
+            value:req.params.value , 
+            title:`Map where ${req.params.param} is ${req.params.value}`,
+            queryNames:queryNames,
+            fieldNames:fieldNames
+        });
         //res.status(200).send(result);
+    });
+});
+
+app.get('/new', function(req, res) {
+    res.render('new.ejs', {
+        queryNames:queryNames,
+        fieldNames:fieldNames
+    });
+});
+
+app.get('/edit/:id', function(req, res) {
+    db.collection('thaidb').findOne({id: req.params.id}, (err, result) => {
+        if (err) return console.log(err);
+        res.render('new.ejs', {
+            data:result,
+            queryNames:queryNames,
+            fieldNames:fieldNames
+        });
     });
 });
